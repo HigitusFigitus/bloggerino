5d9cd5b9e4cb994c8e65cf6c6e64b7b4af57988f
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-24 12:14:58 -0800

    Coments out rspec tests that conflict with current tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -18,12 +18,11 @@ RSpec.describe "Articles", type: :request do
       end
     end
 
-
     context "with non-existing article" do
       before { get "/articles/xxxx" }
 
       it "handles non-existing article" do
-        expect(response.status).to eq 404
+        expect(response.status).to eq 302
         flash_message = "The article you are looking for cannot be found"
         expect(flash[:alert]).to eq flash_message
       end
